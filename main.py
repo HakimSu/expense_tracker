41e27b374f7dab1fa20da80dd649d0543bafdce8
Om Subham Pradhan <omsubhampradhan@gmail.com>
2026-06-26 22:21:52 +0530

    complete program take from om

--- main.py
+++ main.py
@@ -7,4 +7,21 @@ print("4. Category-wise summary" )
 print("5. Exit")
 choice = input("Enter your choice: ")
 today = date.today()
-test = today.strftime("%Y-%m-%d")
+amount = input("Enter amount: ")
+category = input("Enter category: ")
+description = input("Enter description: ")
+
+if choice == "1":
+    from utils import add_expense
+    add_expense(float(amount), category, description)
+elif choice == "2":
+    from utils import view_expenses
+    view_expenses()
+elif choice == "3":
+    from utils import total_spent
+    total_spent()
+elif choice == "4":
+    from utils import category_summary
+    category_summary()
+elif choice == "5":
+    print("Exiting the program.")
