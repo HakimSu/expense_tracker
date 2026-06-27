afc359cfbf91684751c9d53c085d4f95fff9489e
aesthetic-guru-07 <jagannathguru07@gmail.com>
2026-06-27 12:45:20 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-from datetime import date
 print("+-------------------+")
 print("|  Expense tracker  |")
 print("+-------------------+")
 print("1. Add expense")
 print("2. View expenses")
 print("3. Total spent")
-print("4. Category-wise summary" )
+print("4. Category Summary" )
 print("5. Exit")
 while True:
+
     choice = input("Enter your choice: ")
 
-    match(choice):
-        case "1":
-            from utils import add_expense
-            amount = input("Enter amount: ")
-            category = input("Enter category: ")
-            description = input("Enter description: ")
-            add_expense(float(amount), category, description)
-            break
-        case "2":
-            from utils import view_expenses
-            view_expenses()
-            break
-        case "3":
-            from utils import total_spent
-            total_spent()
-            break
-        case "4":
-            from utils import category_summary
-            category_summary()
-            break
-        case "5":
-            print("Exiting the program.")
-            break
-        case _:
-            print("Invalid Choice")
+    if choice == "1":
+        from utils import add_expense
+        amount = input("Enter amount: ")
+        category = input("Enter category: ")
+        description = input("Enter description: ")
+        add_expense(float(amount), category, description)
+
+    elif choice == "2":
+        from utils import view_expenses
+        view_expenses()
+
+    elif choice == "3":
+        from utils import total_spent
+        total_spent()
+
+    elif choice == "4":
+        from utils import category_summary
+        category_summary()
+
+    elif choice == "5":
+        print("Bye !")
+        break
+
+    else:
+        print("Invalid Choice, Choose a valid Input.")
