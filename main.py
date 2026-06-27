e6e0e01cd910cfa4a11c34cd2cf0b2eb960a65b0
aesthetic-guru-07 <jagannathguru07@gmail.com>
2026-06-27 20:52:40 +0530

    Complete

--- main.py
+++ main.py
@@ -1,36 +1,97 @@
+import json
+import datetime
+
+def add_expense(amo, cat, desc):
+    with open ("expenses.json", "r") as file:
+        exps = json.load(file)
+
+    exp = {
+        "date" : str(datetime.date.today()),
+        "amount" : amo,
+        "category" : cat,
+        "description" : desc
+    }
+
+    exps.append(exp)
+
+    with open("expenses.json", "w") as file:
+        json.dump(exps, file, indent = 4)
+
+    print("Successfully Added !")    
+
+def view_expenses():
+    with open("expenses.json", "r") as file:
+        exps = json.load(file)
+
+    if exps:
+        for exp in exps:
+            print(f"Date: {exp["date"]}")
+            print(f"Amount: {exp["amount"]}")
+            print(f"Category: {exp["category"]}")
+            print(f"Description: {exp["description"]}")
+    else:
+        print("No Data about your expenses")
+        return
+
+def total_spent():
+    with open ("expenses.json", "r") as file:
+        exps = json.load(file)
+
+    for exp in exps:
+        total = sum(exp["amount"])
+
+    print(f"Total Expense: {total}")
+
+def category_summary():
+    with open ("expenses.json", "r") as file:
+        exps = json.load(file)
+
+    if exps:
+        print(f"Date        Category  Amount   Description")
+        for exp in exps:
+            print(f"{exp["date"]}    {exp["category"]}    {exp["amount"]}    {exp["description"]}")
+    else:
+        print("You have no Expenses")
+        return
+    
+def delete_expenses():
+    with open ("expenses.json", "w") as file:
+        json.dump({}, file)
+    print("Expenses Deleted Successfully")
+    
 print("+-------------------+")
-print("|  Expense tracker  |")
+print("|  Expense Tracker  |")
 print("+-------------------+")
 print("1. Add expense")
 print("2. View expenses")
 print("3. Total spent")
 print("4. Category Summary" )
-print("5. Exit")
+print("5. Delete all expenses" )
+print("6. Exit")
 while True:
 
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        from utils import add_expense
-        amount = input("Enter amount: ")
+        amount = float(input("Enter amount: "))
         category = input("Enter category: ")
         description = input("Enter description: ")
-        add_expense(float(amount), category, description)
+        add_expense(amount, category, description)
 
     elif choice == "2":
-        from utils import view_expenses
         view_expenses()
 
     elif choice == "3":
-        from utils import total_spent
         total_spent()
 
     elif choice == "4":
-        from utils import category_summary
         category_summary()
 
     elif choice == "5":
-        print("Bye !")
+        delete_expenses()
+        
+    elif choice == "6":
+        print("Exited")
         break
 
     else:
